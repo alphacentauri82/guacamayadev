aa240303ce7476b5e4846893ec42e44045a38e02
root <root@LAPTOP-3QNDH6VP.localdomain>
2020-02-03 22:52:41 -0600

    Added some styles and animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { ScullyRoutesService } from '@scullyio/ng-lib'
 import { map } from 'rxjs/operators';
+import { useAnimation, transition, trigger, style, animate, state } from '@angular/animations';
+import { transAnimation } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('fadeAnimation', [
+      state('in', style({opacity: 1})),
+      transition(':enter', [
+        style({opacity: 0}),
+        animate(600 )
+      ]),
+      transition(':leave',animate(600, style({opacity: 0})))
+    ])
+  ]
 })
+
 export class AppComponent {
   title = 'guacamaya';
   //Sort elements by orders
