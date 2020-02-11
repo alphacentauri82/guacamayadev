d3b3ac3535eb9205fbf02115207b0939946b51e7
root <root@LAPTOP-3QNDH6VP.localdomain>
2020-02-11 00:05:56 -0600

    Solved some bugs in video component.

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -12,50 +12,25 @@ import { ScullyRoutesService } from '@scullyio/ng-lib'
 })
 export class VideosComponent implements OnInit {
 
-  /*videos: any;
-  next: string = "";
-  prev: string = "";*/
+  //episodes: any;
 
-  videos: any;
-  home: any;
-
+  video_pipe$ = this.routerService.available$.pipe(
+    map(routeList => routeList.filter((route) => {
+       return route.route.indexOf("blog/videos") !== -1;
+      }) 
+    )
+  );
+  
   constructor(public routerService: ScullyRoutesService) { }
 
   ngOnInit(): void {
 
-    this.videos = {};
-    this.home = {};
+    //this.episodes = {};
 
-    this.routerService.available$.pipe(
+    /*this.routerService.available$.pipe(
       map(routeList => routeList.filter((routerItem) => { return routerItem.route.indexOf("blog/videos") != -1 }))
     ).subscribe(list => {
-      this.videos = list[0];
-    });
-
-    this.routerService.available$.pipe(
-      map(routeList => routeList.filter((routerItem) => { return routerItem.route.indexOf("blog/home") != -1 }))
-    ).subscribe(list => {
-      this.home = list[0];
-    });
-
-    /*this.videos = [];
-    this.route.queryParams.subscribe(params => {
-
-      let options = {channelId: "UC7WVhpS_lwJot0DBaEnIccg", maxResults: 10};
-      if(params.page !== null && params.page !== undefined)
-        options["page"] = params.page
-      
-      this.youTubeService.getVideos(options)
-        .pipe(takeUntil(this.unsubscribe$))
-        .subscribe(list => {
-          if(list["prevPageToken"] !== null && list["prevPageToken"] !== undefined)
-            this.prev = "/videos?page=" + list["prevPageToken"];
-          if(list["nextPageToken"] !== null && list["nextPageToken"] !== undefined)
-            this.next = "/videos?page=" + list["nextPageToken"];
-          for(let item of list["items"]) {
-            this.videos.push(item);
-          }
-      });
+      this.episodes = list[0];
     });*/
   }
 
